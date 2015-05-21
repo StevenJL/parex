fc471578a291687a0a8ae735819d93af14ea964f
Steven Li <sli@bleacherreport.com>
2015-05-21 12:47:51 -0700

    add basic test

--- test/parex_test.exs
+++ test/parex_test.exs
@@ -1,7 +1,12 @@
 defmodule ParexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it returns results for functions" do
+    results = Parex.parallel_execute([
+      add: fn() -> 1+1 end, 
+      greet: fn() -> "Hi!" end
+    ])       
+    assert Keyword.get(results, :add) == 2
+    assert Keyword.get(results, :greet) == "Hi!"
   end
 end
